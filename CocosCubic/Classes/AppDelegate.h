21276f34de7fb4e9704de9460930019b4424bd29
Onur Atamer <onur.atamer@gmail.com>
2014-07-23 19:48:31 +0300

    Initial Commit

---
+++ CocosCubic/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  CocosCubic
+//
+//  Created by Onur Atamer on 23/07/14.
+//  Copyright Onur Atamer 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ CocosCubic/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CocosCubic
+//
+//  Created by Onur Atamer on 23/07/14.
+//  Copyright Onur Atamer 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
